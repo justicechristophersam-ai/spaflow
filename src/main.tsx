6a87fda5b9a9766abbe1aebd369a0a49b55457ab
Sam <justicechristophersam@gmail.com>
2025-10-21 23:39:29 +0000

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
+import AdminApp from './AdminApp'; // ← new file you’ll add
 import './index.css';
 
+const path = window.location.pathname.replace(/\/+$/, ''); // strip trailing slash
+const isAdminRoute = path === '/admin' || path.startsWith('/admin/');
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    {isAdminRoute ? <AdminApp /> : <App />}
+  </React.StrictMode>
 );
